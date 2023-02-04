a7cca5bda72825bd2603893f82be8da3bebceb3f
zongbeen <hjb825@naver.com>
2023-02-04 21:51:53 +0900

    querydsl

--- src/test/java/com/maven_shopping/repository/ItemRepositoryTest.java
+++ src/test/java/com/maven_shopping/repository/ItemRepositoryTest.java
@@ -3,13 +3,18 @@ package com.maven_shopping.repository;
 import com.maven_shopping.constant.ItemSellStatus;
 import com.maven_shopping.entity.Item;
 import com.maven_shopping.entity.QItem;
+import com.querydsl.core.BooleanBuilder;
 import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.TestPropertySource;
+import org.thymeleaf.util.StringUtils;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -133,5 +138,55 @@ class ItemRepositoryTest {
             System.out.println(item.toString());
         }
     }
+    public void createItemList2() { //데이터 생성을 위해 상품을 저장하는 메서드
+        for(int i=1; i<=5; i++) {
+            Item item = new Item();
+            item.setItemName("테스트 상품" + i);
+            item.setPrice(10000 + i);
+            item.setItemDetail("테스트 상품 상세 설명" + i);
+            item.setItemSellStatus(ItemSellStatus.SELL);
+            item.setStockNum(100);
+            item.setRegTime(LocalDateTime.now());
+            item.setUpdateTime(LocalDateTime.now());
+            itemRepository.save(item);
+        }
+        for(int i=6; i<=10; i++) {
+            Item item = new Item();
+            item.setItemName("테스트 상품" + i);
+            item.setPrice(10000 + i);
+            item.setItemDetail("테스트 상품 상세 설명" + i);
+            item.setItemSellStatus(ItemSellStatus.SOLD_OUT);
+            item.setStockNum(100);
+            item.setRegTime(LocalDateTime.now());
+            item.setUpdateTime(LocalDateTime.now());
+            itemRepository.save(item);
+        }
+    }
+    @Test
+    @DisplayName("상품 Qyerydsl 조회테스트 2")
+    public void queryDslTest2() {
+        this.createItemList2();
+        //쿼리에 들어갈 조건을 만들어주는 빌더 (메소드 체인 형식)
+        BooleanBuilder booleanBuilder = new BooleanBuilder();
+        QItem item = QItem.item;
+        String itemDetail = "테스트 상품 상세 설명";
+        int price = 10003;
+        String itemSellStat = "SELL";
 
+        booleanBuilder.and(item.itemDetail.like("%" + itemDetail + "%"));
+        booleanBuilder.and(item.price.gt(price));
+
+        if(StringUtils.equals(itemSellStat, ItemSellStatus.SELL)) {
+            booleanBuilder.and(item.itemSellStatus.eq(ItemSellStatus.SELL));
+        }
+
+        Pageable pegeable = PageRequest.of(0,5);
+        Page<Item> itemPagingResult = itemRepository.findAll(booleanBuilder, Pageable.unpaged());
+        System.out.println("total elements : " + itemPagingResult.getTotalElements());
+
+        List<Item> resultItemList = itemPagingResult.getContent();
+        for(Item resiltItem: resultItemList) {
+            System.out.println(resiltItem.toString());
+        }
+    }
 }
