9ba7970673b95a833bd041883d276a0519516a88
zongbeen <hjb825@naver.com>
2023-02-06 21:27:14 +0900

    itemControll

--- src/main/java/com/maven_shopping/config/SecurityConfig.java
+++ src/main/java/com/maven_shopping/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -20,20 +21,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     MemberService memberService;
     //http 요청에 대한 보안 설정
     //페이지 권한, 로그인 페이지, 로그아웃 메소드등 설정
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.formLogin()
-                .loginPage("/members/login") //로그인 페이지 URL
-                .defaultSuccessUrl("/") //로그인 성공 시 URL
-                .usernameParameter("email") //로그인 시 사용할 파라미터 이름을 email로 지정
-                .failureUrl("/members/login/error") //로그인 실패 시 URL
-                .and()
-                .logout()
-                .logoutRequestMatcher(new AntPathRequestMatcher
-                        ("/members/logout"))//로그아웃 URL
-                .logoutSuccessUrl("/") //로그아웃 성공 시 URL
-        ;
-    }
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.formLogin()
+//                .loginPage("/members/login") //로그인 페이지 URL
+//                .defaultSuccessUrl("/") //로그인 성공 시 URL
+//                .usernameParameter("email") //로그인 시 사용할 파라미터 이름을 email로 지정
+//                .failureUrl("/members/login/error") //로그인 실패 시 URL
+//                .and()
+//                .logout()
+//                .logoutRequestMatcher(new AntPathRequestMatcher
+//                        ("/members/logout"))//로그아웃 URL
+//                .logoutSuccessUrl("/") //로그아웃 성공 시 URL
+//        ;
+//    }
     //비밀번호 암호화하여 저장
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -49,5 +50,33 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     throws Exception{
         auth.userDetailsService(memberService)
                 .passwordEncoder(passwordEncoder());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.formLogin()
+                .loginPage("/members/login")
+                .defaultSuccessUrl("/")
+                .usernameParameter("email")
+                .failureUrl("/members/login/error")
+                .and()
+                .logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/members/logout"))
+                .logoutSuccessUrl("/")
+            ;
+
+        http.authorizeRequests()
+                .mvcMatchers("/", "/members/**",
+                            "/item/**", "/images/**").permitAll()
+                .mvcMatchers("/admin/**").hasRole("ADMIN")
+                .anyRequest().authenticated()
+                ;
+        http.exceptionHandling()
+                .authenticationEntryPoint(new CustomAuthenticationEntryPoint())
+        ;
+    }
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/css/**", "/js/**", "/img/**");
     }
 }
