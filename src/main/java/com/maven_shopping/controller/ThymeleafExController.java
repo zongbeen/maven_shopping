727c4f01e0e01c15faf56ea6bc407f6c36db062f
zongbeen <hjb825@naver.com>
2023-02-06 19:17:04 +0900

    thymeleaf&html

---
+++ src/main/java/com/maven_shopping/controller/ThymeleafExController.java
@@ -0,0 +1,64 @@
+package com.maven_shopping.controller;
+
+import com.maven_shopping.dto.ItemDto;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequestMapping(value = "/thymeleaf")
+public class ThymeleafExController {
+    @GetMapping(value = "/ex01")
+    public String thymeleafExample01(Model model) {
+        //model 객체를 이용해 뷰에 전달한 데이터를 key, velue로 넣어상
+        model.addAttribute("data", "타임리프 예제 입니다.");
+        return "thymeleafEx/thymeleafEx01";
+    }
+    @GetMapping(value = "/ex02")
+    public String thymeleafExample02(Model model) {
+        ItemDto itemDto = new ItemDto();
+        itemDto.setItemDetail("상품 상세 설명");
+        itemDto.setItemName("테스트 상품1");
+        itemDto.setPrice(10000);
+        itemDto.setRegTime(LocalDateTime.now());
+
+        model.addAttribute("itemDto", itemDto);
+        return "thymeleafEx/thymeleafEx02";
+    }
+
+    @GetMapping(value = "/ex03")
+    public String thymeleafExample03(Model model) {
+        List<ItemDto> itemDtoList = new ArrayList<>();
+
+        for(int i=1; i<=10; i++) {
+            ItemDto itemDto = new ItemDto();
+            itemDto.setItemDetail("상품 상세 설명" + i);
+            itemDto.setItemName("테스트 상품" + i);
+            itemDto.setPrice(10000*i);
+            itemDto.setRegTime(LocalDateTime.now());
+
+            itemDtoList.add(itemDto);
+        }
+        model.addAttribute("itemDtoList", itemDtoList);
+        return "thymeleafEx/thymeleafEx03";
+    }
+    @GetMapping(value = "/ex04")
+    public String thymeleafExample04() {
+        return "thymeleafEx/thymeleafEx04";
+    }
+    @GetMapping(value = "/ex05")
+    public String thymeleafExample05(String param1, String param2, Model model) {
+        model.addAttribute("param1", param1);
+        model.addAttribute("param2", param2);
+        return "thymeleafEx/thymeleafEx05";
+    }
+    @GetMapping(value = "/ex06")
+    public String thymeleafExample06() {
+        return "thymeleafEx/thymeleafEx06";
+    }
+}
---
+++ src/main/java/com/maven_shopping/dto/ItemDto.java
@@ -0,0 +1,18 @@
+package com.maven_shopping.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+public class ItemDto {
+    private Long id;
+    private String itemName;
+    private Integer price;
+    private String itemDetail;
+    private String sellStatCd;
+    private LocalDateTime regTime;
+    private LocalDateTime updateTime;
+}
